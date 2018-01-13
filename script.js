67074f33188ea0c210f67716ccd07065b1b66cde
Agnieszka Adamczyk <adamcz.aga@gmail.com>
2018-01-13 09:59:10 +0100

    Modify script.js

--- script.js
+++ script.js
@@ -1,28 +1,29 @@
-//Zad. 1. 
-const sayHello = (hello = `Hello ` , world = 'World') => console.log(`${hello} ${world}`);
-
+//Zad. 1.
+const sayHello = (hello = `Hello `, world = "World") =>
+	console.log(`${hello} ${world}`);
 
 //Zad.2.
 
-let multiply = (a, b=1) => {return a*b};
-
-//Zad. 3. 
-
-const average = (...args) => {
-  const result = args.reduce( (x, y) => {x + y} );
-  return result/args.length;
+let multiply = (a, b = 1) => {
+	return a * b;
 };
 
+//Zad. 3.
 
-//Zad. 4. 
+const average = (...args) => {
+	const result = args.reduce((x, y) => {
+		x + y;
+	});
+	return result / args.length;
+};
+
+//Zad. 4.
 const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
-const avaerage= (((...args)=> args.reduce((sum, next) => sum + next))(...args)) / grades.length;
-
+const avaerage =
+	((...args) => args.reduce((sum, next) => sum + next))(...args) /
+	grades.length;
 
 //Zad. 5. Destrukturyzacja w celu wyciągnięcia firstname i lastname.OKOK
 
-const data = [1, 4, 'Iwona', false, 'Nowak'];
+const data = [1, 4, "Iwona", false, "Nowak"];
 const [, , firstname, , lastname] = data;
-
-
-
