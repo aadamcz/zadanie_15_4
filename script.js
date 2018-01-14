8a4dd2fa2b28f33a4d16432e1fcd092599d8eb2d
aadamcz <33779500+aadamcz@users.noreply.github.com>
2018-01-14 21:50:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const grades = [1, 5, 5, 5, 4, 3, 3, 2, 1];
 const average = (...grades) =>
 	grades.reduce((sum, next) => sum + next) / grades.length;
 
-//Zad. 5. Destrukturyzacja w celu wyciągnięcia firstname i lastname.OKOK
+//Zad. 5. Destrukturyzacja w celu wyciągnięcia firstname i lastname.
 
 const data = [1, 4, "Iwona", false, "Nowak"];
 const [, , firstname, , lastname] = data;
